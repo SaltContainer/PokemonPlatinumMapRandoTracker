b469dea9bb4cc1d4ec0cc5af66bee377a2c57383
Samuel Lambert <fanmc34@gmail.com>
2022-01-05 17:12:45 -0500

    Saving feature

--- scripts/init.lua
+++ scripts/init.lua
@@ -4,7 +4,10 @@ Tracker:AddItems("items/maps.json")
 Tracker:AddMaps("maps/maps.json")
 
 ScriptHost:LoadScript("scripts/class.lua")
+ScriptHost:LoadScript("scripts/global.lua")
 ScriptHost:LoadScript("scripts/custom_item.lua")
+ScriptHost:LoadScript("scripts/savestorage.lua")
+
 ScriptHost:LoadScript("scripts/doors/doorslotselect.lua")
 ScriptHost:LoadScript("scripts/doors/doorslothub.lua")
 ScriptHost:LoadScript("scripts/doors/doorslot.lua")
@@ -17,3 +20,5 @@ Tracker:AddLayouts("layouts/map_tabs.json")
 Tracker:AddLayouts("layouts/warp_icons.json")
 
 Tracker:AddLayouts("layouts/tracker.json")
+
+SaveStorage()
